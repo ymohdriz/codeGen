11e1028475f51d935cc9fbd5a866e0021a33876d
Mohammed Rizwan <mriz@softwareag.com>
2024-04-29 20:31:40 +0530

    Initial commit

---
+++ src/test/java/com/mcode/llp/codeGen/CodeGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcode.llp.codeGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
