bc9dbfb0d7233fd5ad281eaf052db6da2dde9bf2
Mohammed Yasar S <yasarmohamed55@gmail.com>
2024-10-14 20:19:37 +0530

    Revert "ADD EXCEPTION"
    This reverts commit fee7b4151851ba55a8d56c8978e0fe4c3ea78aef.

--- src/main/java/com/mcode/llp/codeGen/CodeGenApplication.java
+++ src/main/java/com/mcode/llp/codeGen/CodeGenApplication.java
@@ -4,6 +4,7 @@ import com.mcode.llp.codeGen.models.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppConfig.class)
